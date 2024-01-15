68a5c7cdefb26b3e2b041f142a9a86a693fc3504
Stephen Freeman <sfreeman@suns.com>
2024-01-15 16:15:14 -0700

    added header and styled components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import './Movies';
-import SideNav from './Component/SideNav/SideNav';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./Movies";
+import SideNav from "./Component/SideNav/SideNav";
+import Header from "./Component/SideNav/Header/Header";
 
 const headers = {
-    'X-RapidAPI-Key': 'fd7c953c4amshfa5b3215ca99ba0p1a522djsn7bafc29ea657',
-    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-  
+  "X-RapidAPI-Key": "fd7c953c4amshfa5b3215ca99ba0p1a522djsn7bafc29ea657",
+  "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
 };
 
 function App() {
-  const [data, setData] = useState([])
-  
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
-        .get('https://moviesdatabase.p.rapidapi.com/titles', {
-          headers:headers
-        })
-        .then(response => {
-            setData(response.data)
-        })
-        .catch(error => {
-          console.error('server error', error);
-        })
-  }, [])
-  console.log(data)
+      .get("https://moviesdatabase.p.rapidapi.com/titles", {
+        headers: headers,
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("server error", error);
+      });
+  }, []);
+  console.log(data);
   return (
     <div className="App">
       <SideNav />
-      <div className='right-content'>
-
+      <div className="right-content">
+        <Header />
       </div>
     </div>
-
   );
 }
 
